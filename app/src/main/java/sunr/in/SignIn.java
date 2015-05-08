ce98e6ff9aab2077c1193d6d0824acbaf9e9b795
LNTCS <lntcs.dev@gmail.com>
2015-05-08 14:01:34 +0900

    fu**ing fb SDK >_<

--- app/src/main/java/sunr/in/SignIn.java
+++ app/src/main/java/sunr/in/SignIn.java
@@ -1,31 +1,63 @@
 package sunr.in;
 
+import android.content.Context;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.RelativeLayout;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 /**
  * Created by LNTCS on 2015-05-07.
  * Project-Juilliard
  */
-public class SignIn extends AppCompatActivity{
+public class SignIn extends AppCompatActivity implements View.OnClickListener{
     RelativeLayout fbLay;
+    LoginButton loginButton;
+    CallbackManager callbackManager;
+    Context mContext;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
         setContentView(R.layout.activity_signin);
+
+        mContext = this;
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.actionbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         fbLay = (RelativeLayout) findViewById(R.id.signin_lay_fb);
         ((GradientDrawable)fbLay.getBackground()).setColor(getResources().getColor(R.color.facebook));
 
+        loginButton= (LoginButton) findViewById(R.id.login_button);
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                    }
+                    @Override
+                    public void onCancel() {
+                    }
+                    @Override
+                    public void onError(FacebookException exception) {
+                    }
+                });
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()){
@@ -34,6 +66,11 @@ public class SignIn extends AppCompatActivity{
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
         }
     }
 }
