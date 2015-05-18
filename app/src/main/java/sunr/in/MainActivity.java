efdccfbc097ad7dbf5200cb50fdcd0b28a8b72ad
LNTCS <lntcs.dev@gmail.com>
2015-05-18 14:38:26 +0900

    before add number picker

--- app/src/main/java/sunr/in/MainActivity.java
+++ app/src/main/java/sunr/in/MainActivity.java
@@ -1,21 +1,33 @@
 package sunr.in;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.GridView;
+import android.widget.TextView;
 
 import com.balysv.materialmenu.MaterialMenuDrawable;
 import com.balysv.materialmenu.extras.toolbar.MaterialMenuIconToolbar;
 
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import sunr.in.util.BaseActivityHelper;
 import sunr.in.util.MainGridAdapter;
+import sunr.in.widget.RoundImage;
 
 /**
  * Created by LNTCS on 2015-05-07.
@@ -28,24 +40,44 @@ public class MainActivity extends AppCompatActivity {
     private MaterialMenuIconToolbar materialMenu;
     protected BaseActivityHelper helper;
     LayoutInflater inflater;
+    TextView profileName;
     GridView mainGrid;
+    RoundImage profileImg;
+
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mContext = MainActivity.this;
+        pref = getSharedPreferences("Sunr.in", Context.MODE_PRIVATE);
+        editor = pref.edit();
         initLay();
-    }
+        startActivity(new Intent(mContext, SignIn.class));   //Start Sign in for test
 
+    }
+    private TextView getActionBarTextView(Toolbar mToolBar) {
+        TextView titleTextView = null;
+
+        try {
+            Field f = mToolBar.getClass().getDeclaredField("mTitleTextView");
+            f.setAccessible(true);
+            titleTextView = (TextView) f.get(mToolBar);
+        } catch (NoSuchFieldException e) {
+        } catch (IllegalAccessException e) {
+        }
+        return titleTextView;
+    }
     public void initLay(){
         inflater = LayoutInflater.from(mContext);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.actionbar);
+
+        profileName = (TextView) findViewById(R.id.main_side_name);
         mainGrid = (GridView) findViewById(R.id.main_grid);
-
+        profileImg = (RoundImage) findViewById(R.id.img_main_profile);
         setSupportActionBar(toolbar);
-
-//        startActivity(new Intent(mContext, SignIn.class));   //Start Sign in for test
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
         materialMenu = new MaterialMenuIconToolbar(this, Color.GRAY, MaterialMenuDrawable.Stroke.THIN) {
             @Override
             public int getToolbarViewId() {
-                return R.id.toolbar;
+                return R.id.actionbar;
             }
         };
         materialMenu.setNeverDrawTouch(true);
@@ -66,5 +98,33 @@ public class MainActivity extends AppCompatActivity {
         for(int i = 0 ; i < 6; i++) items.add(i);
         MainGridAdapter gridAdapter = new MainGridAdapter(mContext,items);
         mainGrid.setAdapter(gridAdapter);
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/Ubuntu-M.ttf");
+        getActionBarTextView(toolbar).setTypeface(face);
+
+        String profileID = pref.getString("FB_ID", "");
+        if(!profileID.equals("")) new getFacebookProfilePicture().execute(profileID);
+        profileName.setText(pref.getString("FB_Name","Name"));
+    }
+    class getFacebookProfilePicture extends AsyncTask<String,String,String>{
+        Bitmap bitmap;
+        @Override
+        protected String doInBackground(String... params) {
+            URL imageURL = null;
+            try {
+                imageURL = new URL("https://graph.facebook.com/" + params[0] + "/picture?type=large");
+                bitmap = BitmapFactory.decodeStream(imageURL.openConnection().getInputStream());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            profileImg.setImageBitmap(bitmap);
+        }
     }
 }
+
--- app/src/main/java/sunr/in/SignIn.java
+++ app/src/main/java/sunr/in/SignIn.java
@@ -2,6 +2,7 @@ package sunr.in;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
@@ -28,6 +29,7 @@ import com.facebook.Profile;
 import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.getbase.floatingactionbutton.FloatingActionButton;
 
 import sunr.in.widget.MultiSelectSpinner;
 
@@ -39,21 +41,26 @@ public class SignIn extends AppCompatActivity implements View.OnClickListener{
     RelativeLayout fbLay;
     LoginButton loginButton;
     ImageView doneImg,indicateImg;
-    TextView statusTv,graduTv,undergraduTv;
+    TextView statusTv,graduTv,undergraduTv,hakbunTv;
     CallbackManager callbackManager;
     Context mContext;
     ProfileTracker profileTracker;
     Spinner majorSpn;
     MultiSelectSpinner branchSpn;
     boolean isGraduate = true;
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    FloatingActionButton fabDone;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
+        pref = getSharedPreferences("Sunr.in", Context.MODE_PRIVATE);
+        editor = pref.edit();
+
         setContentView(R.layout.activity_signin);
         mContext = this;
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.actionbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -110,6 +117,17 @@ public class SignIn extends AppCompatActivity implements View.OnClickListener{
                     isGraduate = true;
                 }
                 break;
+            case R.id.signin_fab_ok:
+                Profile profile = Profile.getCurrentProfile();
+                if(AccessToken.getCurrentAccessToken() != null && profile != null){
+                    editor.putString("FB_ID",profile.getId());
+                    editor.putString("FB_Name",profile.getName());
+                    editor.commit();
+                }
+                break;
+            case R.id.signin_hakbun:
+                
+                break;
         }
     }
     public void initLayout(){
@@ -122,6 +140,8 @@ public class SignIn extends AppCompatActivity implements View.OnClickListener{
         loginButton = (LoginButton) findViewById(R.id.login_button);
         majorSpn = (Spinner) findViewById(R.id.signin_spn_major);
         branchSpn = (MultiSelectSpinner) findViewById(R.id.signin_spn_branch);
+        fabDone = (FloatingActionButton) findViewById(R.id.signin_fab_ok);
+        hakbunTv = (TextView) findViewById(R.id.signin_hakbun);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter
                 .createFromResource(this, R.array.major_array,R.layout.spinner_item);
@@ -155,6 +175,8 @@ public class SignIn extends AppCompatActivity implements View.OnClickListener{
         fbLay.setOnClickListener(this);
         graduTv.setOnClickListener(this);
         undergraduTv.setOnClickListener(this);
+        fabDone.setOnClickListener(this);
+        hakbunTv.setOnClickListener(this);
     }
 
     public void updateUI(){
